d37c881e3d970901f00ece6995dc3966c57205e7
Martin <martintino90909@gmail.com>
2021-03-11 18:27:20 +0000

    password generator function

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("hello")
+import random
+import string
+
+def generate_password(length):
+    password = ""
+    for i in range(length):
+        password += random.choice(string.ascii_lowercase)
+    return password
+
+print(generate_password(17))

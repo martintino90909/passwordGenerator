01e4c37f899afe1cb6f77d360d49e57348b11131
Martin <martintino90909@gmail.com>
2021-03-12 18:30:26 +0000

    password generator function

---
+++ src/my torn 2.py
@@ -0,0 +1,4 @@
+import string
+
+characters = string.ascii_letters + string.punctuation + string.digits
+password = join.choice
--- src/my torn.py
+++ src/my torn.py
@@ -9,7 +9,10 @@ def generate_password(length):
     return password
 
 
-default_length = 20
-user_input = input("Please enter password length [" + str(default_length) + "] : ") or default_length
+default_length = 8
+try:
+    user_input = input('Please enter password length: ')
+except:
+    user_input = default_length
 
 print(generate_password(user_input))

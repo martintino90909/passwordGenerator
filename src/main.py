f0d25b14ffcf4dd5c6ecb10d32c48c428713b2a2
Martin <martintino90909@gmail.com>
2021-03-12 20:26:06 +0000

    save to file check user input

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import random
 import string
 
+default_length = 20
+
 
 def generate_password(length):
     password = ""
@@ -9,7 +11,24 @@ def generate_password(length):
     return password
 
 
-default_length = 20
-user_input = input("Please enter password length [" + str(default_length) + "] : ") or default_length
+def get_correct_user_input(message):
+    while True:
+        try:
+            user_input = int(input(message) or default_length)
+        except ValueError:
+            print("Please enter a integer")
+            continue
+        else:
+            break
+    return user_input
 
-print(generate_password(user_input))
+
+msg = "Please enter password length [" + str(default_length) + "] : "
+user_int_input = get_correct_user_input(msg)
+output = generate_password(user_int_input)
+print(output)
+
+
+password_file = open('password.txt','a')
+password_file.write(output +"\n")
+password_file.close()
